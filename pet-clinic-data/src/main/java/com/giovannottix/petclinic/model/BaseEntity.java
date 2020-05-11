b37b7a97c14a256a86048757baa9499258667e90
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-10 21:59:37 -0500

    Implement Base Entity (JPA). Closes #55

--- pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/BaseEntity.java
+++ pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/BaseEntity.java
@@ -1,15 +1,21 @@
 package com.giovannottix.petclinic.model;
 
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
 /**
  *
- *
  * @author: Giovanni Esposito.
  * @Date  : 05/03/20, Sun
  */
+@MappedSuperclass
 public class BaseEntity implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     public Long getId() {
