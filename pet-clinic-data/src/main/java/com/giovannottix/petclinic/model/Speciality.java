78a32ae173e02d775b90e65e420da717ec97ac49
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-10 22:22:33 -0500

    Create Spring Data JPA Service to Vets. Closes #61 Create Spring Data JPA Service to Speciality. Closes #64

--- pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/Speciality.java
+++ pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/Speciality.java
@@ -1,11 +1,18 @@
 package com.giovannottix.petclinic.model;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
 /**
  * @author: Giovanni Esposito.
  * @Date : 05/09/20, Sat
  */
+@Entity
+@Table(name = "specialties")
 public class Speciality extends BaseEntity{
 
+    @Column(name = "description")
     private String description;
 
     public String getDescription() {
