1d3699897c2e7e2b8d08d59715f1995a1d4cdd64
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-13 20:52:05 -0500

    Defensive coding when save Visit

--- pet-clinic-data/src/main/java/com/giovannottix/petclinic/services/jpa/VisitSpringDataJPAService.java
+++ pet-clinic-data/src/main/java/com/giovannottix/petclinic/services/jpa/VisitSpringDataJPAService.java
@@ -32,6 +32,13 @@ public class VisitSpringDataJPAService implements VisitService {
 
     @Override
     public Visit save(Visit visit) {
+        if(visit.getPet() == null
+                || visit.getPet().getOwner() == null
+                || visit.getPet().getId() == null
+                || visit.getPet().getOwner().getId() == null){
+            throw new RuntimeException("Invalid Visit");
+        }
+
         return visitRepository.save(visit);
     }
 
