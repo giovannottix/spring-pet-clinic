aae85a61953cb2e2845be65cf79d55526f0f839b
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-17 19:11:53 -0500

    Add configuration to use JUnit 5, but the SpringBoot 2.2.6 already include it. Closes #4

--- pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/Vet.java
+++ pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/Vet.java
@@ -1,9 +1,6 @@
 package com.giovannottix.petclinic.model;
 
-import lombok.Builder;
-import lombok.Getter;
-import lombok.NoArgsConstructor;
-import lombok.Setter;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.HashSet;
@@ -17,6 +14,7 @@ import java.util.Set;
 @Setter
 @Getter
 @NoArgsConstructor
+@AllArgsConstructor
 @Builder
 @Entity
 @Table(name = "vets")
