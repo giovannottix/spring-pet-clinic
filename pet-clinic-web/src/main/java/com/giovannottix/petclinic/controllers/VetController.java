53fc3299a4de49d8488683c5fedbc856bb1c686d
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-24 13:36:57 -0500

    Implement Vets list view JSON. Closes #107

--- pet-clinic-web/src/main/java/com/giovannottix/petclinic/controllers/VetController.java
+++ pet-clinic-web/src/main/java/com/giovannottix/petclinic/controllers/VetController.java
@@ -1,9 +1,14 @@
 package com.giovannottix.petclinic.controllers;
 
+import com.giovannottix.petclinic.model.Vet;
 import com.giovannottix.petclinic.services.VetService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.Set;
 
 /**
  * @author: Giovanni Esposito.
@@ -25,4 +30,8 @@ public class VetController {
         return "vets/index";
     }
 
+    @GetMapping("api/vets")
+    public @ResponseBody Set<Vet> getVetsJson( ) {
+        return vetService.findAll();
+    }
 }
