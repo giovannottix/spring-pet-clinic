4e71ec0c8a07daf1652c8fd7c0bd73d6e02a923b
Giovanni Esposito <giovanni.esposito@smartmatic.com>
2020-05-23 19:05:47 -0500

    Add and update Pets. Closes #87

--- pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/PetType.java
+++ pet-clinic-data/src/main/java/com/giovannottix/petclinic/model/PetType.java
@@ -14,14 +14,22 @@ import javax.persistence.Table;
 @Setter
 @Getter
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @ToString
 @Table(name = "types")
 public class PetType extends BaseEntity {
 
+    @Builder
+    public PetType(Long id, String name) {
+        super(id);
+        this.name = name;
+    }
+
     @Column(name = "name")
     private String name;
 
+    @Override
+    public String toString() {
+        return name;
+    }
 }
